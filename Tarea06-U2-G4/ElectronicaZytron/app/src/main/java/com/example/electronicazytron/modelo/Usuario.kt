db1a66017773224c516ae6ecdf3f5d2a3184d8c2
SumonPB <bpcondolo@uce.edu.ec>
2025-12-14 23:27:01 -0500

    files.update

--- Tarea06-U2-G4/ElectronicaZytron/app/src/main/java/com/example/electronicazytron/modelo/Usuario.kt
+++
@@ -1,6 +0,0 @@
-package com.example.electronicazytron.modelo
-//clase del objeto usuario con su nombre y apellido
-     data class Usuario(
-         var nombre: String,
-         var apellido: String,
-     )
